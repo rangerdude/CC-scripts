1d827a91c9a2392783c967ed0ee0c216afb80c81
bprice <rangerdude.utah@gmail.com>
2019-12-22 02:34:57 -0700

    change goto2 to goto

--- scripts/planter.lua
+++ scripts/planter.lua
@@ -6,12 +6,14 @@ function plant()
 
 print(start)
 
-	for i=1,25 do
+	for i=1,5 do
 	turtle.forward()
 	turtle.digDown()
 	turtle.select(3)
 	turtle.placeDown()
     end
+
+dofile("goto.lua" start)
 
 end
 
